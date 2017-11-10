9fd6c7f8c44e861de2f5a4b69382a17d93954e04
Gerd Friemel <gerd.friemel@gmail.com>
2017-11-10 20:37:07 +0100

    Unison sync uses global ssh variables.

--- server-sync.sh
+++ server-sync.sh
@@ -7,7 +7,7 @@ mode="git"
 cwd=$(pwd)
 force=0
 
-confdir="${HOME}/.sync-conf-test"
+confdir="${HOME}/.sync-conf"
 
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
